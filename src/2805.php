b29c1bde88dc8568d96e028440bae2ede8c9b335
Ari <ari.neto@estudante.ifto.edu.br>
2025-06-12 14:52:37 -0300

    HTML

---
+++ src/2805.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Bootstrap w/ Vite</title>
+  <script type="module" src="./js/main.js"></script>
+</head>
+
+<body>
+
+
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Gestão Acadêmica</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="disciplina.html">Disciplinas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="professor.html">Professores</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="Turmas.html">Turmas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="livros.html">Livros</a>
+          </li>
+          
+          <li class="nav-item">
+            <a class="nav-link" href="cadastroGrade.html">Grade</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cadastroCurso.html">Curso</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="cadastroUsuario.html">Usuários</a>
+          </li>
+        </ul>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  </div>
+  </form>
+  </form>
+
+  </main>
+  </main>
+</body>
+
+</html>
---
+++ src/scss/Trabalho2805.php
@@ -0,0 +1,78 @@
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap w/ Vite</title>
+    <script type="module" src="./js/main.js"></script>
+  </head>
+  <body>
+    
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Gestão Acadêmica</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+           <li class="nav-item">
+            <a class="nav-link" href="cadastroUsuario.html">Usuários</a>
+          </li>
+        </ul>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+<nav>
+
+
+
+ 
+
+
+</nav>
+
+
+
+<main class="container">
+  
+  <h1>Registrar Usuário</h1>
+
+  <form action="">
+
+    <div class="">
+
+    <div class="mb-1 col-sm-12 col-md-6">
+      <label for="nome" class="form-label">Usuário</label>
+      <input type="text" class="form-control" id="Usuário" placeholder="Insira seu Usuário">
+    </div>
+
+    <div class="mb-1 col-sm-12 col-md-6">
+      <label for="nome" class="form-label">E-mail</label>
+      <input type="text" class="form-control" id="Usuário" placeholder="Insira seu E-mail">
+    </div>
+
+    <div class="mb-1 col-sm-12 col-md-6">
+      <label for="professor" class="form-label">Senha</label>
+      <input type="text" class="form-control" id="senha" placeholder=" Digite sua senha">
+    </div>
+
+    <div class="mb-1 col-sm-12">
+      <button type="submit" class="btn btn-primary">Cadastrar</button>
+    </div>
+  
+      
+  </div>
+  </form>
+
+</main>
+  </body>
+</html>
+
